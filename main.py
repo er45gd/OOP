639eb0bea0758994b4fb57cea4dae415523483f2
er45gd <ethan.r.huddleston@gmail.com>
2026-08-28 09:02:36 -0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,17 @@
+print ("please enter the value of A:")
+a = int(input())
+
+print ("please enter the value of B:")
+b = int(input())
+
+
+#addition
+c = a + b
+print("the sum of the two values is:", c)
+
+#Subtraction
+c = a + b
+print("the difference of the two values is:", c)
+
+#Multiplication
+#Division
---
+++ pp-1.py
@@ -0,0 +1,13 @@
+print ("Input employee's name:")
+name = input()
+
+print ("what is their pay:")
+pay = int(input())
+
+print ("what are their deductions:")
+deducts = int(input())
+totalPay = pay - deducts
+
+print(name)
+print("total pay of:", totalPay)
+
