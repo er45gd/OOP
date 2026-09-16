d3668688abab298aa84b8d8f4161e58495f52130
er45gd <ethan.r.huddleston@gmail.com>
2026-09-16 09:50:19 -0500

    Add files via upload

---
+++ dictionPractice,py.py
@@ -0,0 +1,11 @@
+courses={}
+print("how many courses are you taking")
+coursesNumber=int(input())
+i=0
+l=1
+while coursesNumber!=i:
+    print("name of class number", l)
+    courseName=input()
+    i=i+1
+    l=l+1
+
